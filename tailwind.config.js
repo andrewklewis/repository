48dbfa154a3e3aace7428eec476579f1b78149ca
Your Name <your-email@example.com>
2024-12-23 14:03:46 -0600

    changes

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html",    // Scans the main HTML file
+    "./src/**/*.css"   // Scans all CSS files in the 'src' folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
